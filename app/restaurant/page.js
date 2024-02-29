75708fe12e9473c2ae9ba821557b0d5a338a77fb
Mobina <mobina.ysf203@gmail.com>
2024-02-29 23:50:00 +0330

    fix (header services): fixed page urls on navbar HeaderServices

---
+++ app/restaurant/page.js
@@ -0,0 +1,7 @@
+export default function restaurant () {
+    return (
+        <div>
+            sdfjsdf
+        </div>
+    )
+}
--- components/header/HeaderServices.jsx
+++ components/header/HeaderServices.jsx
@@ -1,5 +1,6 @@
 import {getData } from "@/lib/dataFeching";
 import Image from "next/image";
+import Link from "next/link";
 
 export const HeaderServices = async () => {
 
@@ -9,10 +10,12 @@ export const HeaderServices = async () => {
         <nav className={"flex flex-nowrap pt-4 overflow-x-auto"}>
             {storeCategories.map( (item) => (
                 <div className={"grow"} key={item.id}>
-                    <div className={"flex flex-col items-center justify-center pb-4 min-w-[5.5rem]"}>
-                        <Image className={"mb-4"} width={40} height={40} src={item.icon} alt={item.name}/>
-                        <span className={"font-iranSans text-xs text-carbon-light"}>{item.name}</span>
-                    </div>
+                    <Link href={item.url}>
+                        <div className={"flex flex-col items-center justify-center pb-4 min-w-[5.5rem]"}>
+                            <Image className={"mb-4"} width={40} height={40} src={item.icon} alt={item.name}/>
+                            <span className={"font-iranSans text-xs text-carbon-light"}>{item.name}</span>
+                        </div>
+                    </Link>
                 </div>
             ))}
         </nav>
