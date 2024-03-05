64ec6455269b079cd7c55bb661c8e88284caad4e
Mobina <mobina.ysf203@gmail.com>
2024-03-05 03:47:40 +0330

    fix (headerAddressSlice) : fixed initial state address

--- redux/features/headerAddressSlice.js
+++ redux/features/headerAddressSlice.js
@@ -5,7 +5,7 @@ export const headerAddressSlice = createSlice({
     name : "addressModal",
     initialState : {
         isOpen : false,
-        address : "تهران، کشاورز - پارک لاله، ولی عصر، فلسطین، برادران فرزام قبل از مهدوی"
+        address : "تهران، کشاورز - پارک لاله، ولی عصر، فلسطین، برادران فرزام قبل از مهدوی ۱"
     },
     reducers : {
         openAddressModal : (state) => {
