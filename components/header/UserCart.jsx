e6a3ce0e02da760090282ba72f889e9cb823f2ff
Mobina <mobina.ysf203@gmail.com>
2024-03-05 05:09:19 +0330

    add (UserCart-header): added cart btn to header

---
+++ components/header/UserCart.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export const UserCart = ({user}) => {
+    return (
+        <div className={"mr-4 relative cursor-pointer flex"}>
+            <Image src={"/icons/cart.svg"} width={20} height={20} alt={"icon"} className={"ml-2"}/>
+            <p className={"text-carbon-main text-sm font-iRANSansBold ml-2 max-[959px]:hidden"}>سفارش{"\u200c"}ها</p>
+        </div>
+    )
+}
