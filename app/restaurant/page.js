6264ca1cc0ac81c440b07dbc4298584483d9914a
Mobina <mobina.ysf203@gmail.com>
2024-03-12 23:01:52 +0330

    add (AsideMenu): added filter aside menu for resPage

--- app/restaurant/page.js
+++ app/restaurant/page.js
@@ -1,12 +1,15 @@
 import {getData} from "@/lib/dataFeching";
-import {PageSorting} from "@/components";
+import {AsideMenu, PageSorting} from "@/components";
 
 export default async function restaurant () {
-        const { restaurants, categories, fastFoods, irani, kebab, } = await getData();
+        const { restaurants, resPageCategory, fastFoods, irani, kebab, } = await getData();
 
     return (
         <main className={"p-6 grow w-full max-w-[85.375rem] mx-auto"}>
             <PageSorting/>
+            <div className={"flex-wrap w-[calc(100%+2rem)] m-[calc(-1rem)] flex"}>
+                <AsideMenu category={resPageCategory} fastFoods={fastFoods} irani={irani} kebab={kebab}/>
+            </div>
         </main>
     )
 }
---
+++ components/restaurantPage/aside menu/AsideMenu.jsx
@@ -0,0 +1,15 @@
+import {FoodFilter, PriceFilter, ResFilter} from "@/components";
+
+export const AsideMenu = ({category, fastFoods, irani, kebab}) => {
+    return (
+        <aside className={"aside-resPage basis-full max-w-full p-[calc(1rem)]"}>
+            <div className={"ease-in-out duration-[350ms] transition-[top] sticky top-[5.5rem]"}>
+                <nav className={"flex flex-col min-h-[9.375rem]"}>
+                    <FoodFilter category={category}/>
+                    <PriceFilter/>
+                    <ResFilter/>
+                </nav>
+            </div>
+        </aside>
+    )
+}
