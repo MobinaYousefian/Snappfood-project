8f0814e776ee5693d84649a70ec419449ef3d2e7
Mobina <mobina.ysf203@gmail.com>
2024-03-16 00:16:31 +0330

    add (Restaurant menu): added restaurant menu page for each restaurant

--- app/restaurant/[...restaurantId]/page.js
+++ app/restaurant/[...restaurantId]/page.js
@@ -1,6 +1,16 @@
-export default function RestaurantId ({params}) {
-    console.log(params)
+import {RestaurantInfo} from "@/components";
+import {getData} from "@/lib/dataFeching";
+
+export default async function RestaurantId ({params}) {
+    const pageId = params.restaurantId[1];
+    console.log(pageId)
+    const {restaurants, restaurantsFoods} = await getData();
+
     return (
-        <h1>Hello world! {params.restaurantId[1]}</h1>
+        <main className={"mx-auto max-w-[85.375rem] w-full grow p-4 pt-[4.25rem]"}>
+            <div className={"m-[calc(-1rem)] flex-wrap flex w-[calc(100%+2rem)]"}>
+                <RestaurantInfo restaurants={restaurants}/>
+            </div>
+        </main>
     )
 }
