865ba1c232f78e15a0354eb8ca5524eceb37951d
Mobina <mobina.ysf203@gmail.com>
2024-03-16 01:14:26 +0330

    fix ([...restaurant] page): fixed data fetching and filter each restaurant data

--- app/restaurant/[...restaurantId]/page.js
+++ app/restaurant/[...restaurantId]/page.js
@@ -2,14 +2,14 @@ import {RestaurantInfo} from "@/components";
 import {getData} from "@/lib/dataFeching";
 
 export default async function RestaurantId ({params}) {
+    const {restaurants} = await getData();
     const pageId = params.restaurantId[1];
-    console.log(pageId)
-    const {restaurants, restaurantsFoods} = await getData();
+    const resInfo = restaurants.filter(({id}) => id == pageId)[0]
 
     return (
         <main className={"mx-auto max-w-[85.375rem] w-full grow p-4 pt-[4.25rem]"}>
             <div className={"m-[calc(-1rem)] flex-wrap flex w-[calc(100%+2rem)]"}>
-                <RestaurantInfo restaurants={restaurants}/>
+                <RestaurantInfo resInfo={resInfo}/>
             </div>
         </main>
     )
