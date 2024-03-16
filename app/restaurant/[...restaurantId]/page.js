9437144709966efd3e933b77ed24a64793dfcfb9
Mobina <mobina.ysf203@gmail.com>
2024-03-16 18:48:11 +0330

    fix ([...restaurant] page): try fixed deploy err

--- app/restaurant/[...restaurantId]/page.js
+++ app/restaurant/[...restaurantId]/page.js
@@ -4,7 +4,7 @@ import {getData} from "@/lib/dataFeching";
 export async function generateMetadata ({ params }) {
     const pageId = params.restaurantId[1];
     const {restaurants} = await getData();
-    const resInfo = restaurants.filter(({id}) => id == pageId)[0]
+    const resInfo = restaurants.filter(({id}) => id === pageId)[0]
     return {
         title : `${resInfo.name}-سفارش آنلاین غذا از ${resInfo.name} | اسنپ فود`
     }
@@ -13,7 +13,7 @@ export async function generateMetadata ({ params }) {
 export default async function RestaurantId ({params}) {
     const {restaurants} = await getData();
     const pageId = params.restaurantId[1];
-    const resInfo = restaurants.filter(({id}) => id == pageId)[0]
+    const resInfo = restaurants.filter(({id}) => id === pageId)[0]
 
     return (
         <main className={"bg-surface-main mx-auto max-w-[85.375rem] w-full grow p-4 pt-[4.25rem]"}>
