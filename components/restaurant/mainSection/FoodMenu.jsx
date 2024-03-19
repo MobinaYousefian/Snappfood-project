592c72349b000edc6c7b753f3fa6dfac503af811
Mobina <mobina.ysf203@gmail.com>
2024-03-19 06:20:03 +0330

    add (CouponSection): added CouponSection to each res page

--- components/restaurant/mainSection/FoodMenu.jsx
+++ components/restaurant/mainSection/FoodMenu.jsx
@@ -1,10 +1,13 @@
-import {DiscountSection, MenuSection} from "@/components";
+import {CouponSection, DiscountSection, MenuSection} from "@/components";
 import {PopularSection} from "@/components/restaurant/mainSection/PopularSection";
 
 export const FoodMenu = ({resInfo}) => {
     return (
         <section className={"FoodMenu basis-full max-w-full p-[calc(1rem)]"}>
             <section className={"border-surface-dark bg-surface-light rounded-lg border-[0.0625rem]"}>
+                {
+                    resInfo.couponList && <CouponSection resInfo={resInfo}/>
+                }
                 <PopularSection resInfo={resInfo}/>
                 {
                     resInfo.discountNumber && <DiscountSection resInfo={resInfo}/>
