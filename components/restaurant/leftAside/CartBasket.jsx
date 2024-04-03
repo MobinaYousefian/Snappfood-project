2ecca3219b464dcdfdc579edc04d353e3d7a53e0
Mobina <mobina.ysf203@gmail.com>
2024-04-03 23:23:51 +0330

    chore (CartBill): added CartBill to clean Up codes

--- components/restaurant/leftAside/CartBasket.jsx
+++ components/restaurant/leftAside/CartBasket.jsx
@@ -5,24 +5,14 @@ import {handleOpenWarning} from "@/redux/features/cartSlice";
 import {Suspense, useEffect, useRef} from "react";
 import Loading from "@/app/loading";
 import {priceFormatting, toFarsiNumber} from "@/utils/numberConverter";
-import {CartFoodItem} from "@/components";
+import {CartBill, CartFoodItem} from "@/components";
 import {openAddressModal} from "@/redux/features/headerAddressSlice";
-import clsx from "clsx";
 
 export const CartBasket = ({resInfo}) => {
     const dispatch = useDispatch();
     const {basket} = useSelector(state => state.cart);
-    const {activeList} = useSelector(state => state.activeCoupon);
     const {selected} = useSelector(state => state.addressModal)
-    const activeCouponRef = useRef(null);
     const leftPrice = useRef(null);
-
-    /* to check if there`s any active coupon on the cart*/
-    const couponStr = resInfo.couponList?.map((item) => `${item.discount}/${item.title}`)
-    let isaActiveCoupon = couponStr?.filter((coupon) => activeList.find((item) => item === coupon))[0];
-
-    /* to randomize a number for club points */
-    const randomClubPoint = Math.floor(Math.random() * 1000);
 
     /* to calculate "مجموع" */
     const totalPrice = basket.reduce((prev, cur) => {
@@ -53,24 +43,6 @@ export const CartBasket = ({resInfo}) => {
         }
     }, 0)
 
-    const asidePrice = resInfo.tax + resInfo.packagePrice + resInfo.delivery.price;
-    /* to calculate "قابل پرداخت"*/
-    const finalPrice = basket.reduce((prev, cur) => {
-        const foodPrice = cur.food.price.filter((item) => item.tag === cur.priceTag)[0];
-        if (totalDiscount > 0){
-            return prev + asidePrice + (totalDiscount * cur.counter)
-        } else {
-            return prev + asidePrice + (foodPrice.value * cur.counter)
-        }
-
-    }, 0)
-
-    const handleRemoveCoupon = (e) => {
-        e.preventDefault()
-        if (activeCouponRef.current) {
-            activeCouponRef.current.style.display = "none"
-        }
-    }
 
     const openWarning = (e) => {
         e.preventDefault()
@@ -109,65 +81,12 @@ export const CartBasket = ({resInfo}) => {
                         ))
                     }
                     <Suspense fallback={<Loading/>}>
-                        <div className={"border-b border-b-carbon-alphaLight flex flex-col"}>
-                            <div className={"h-8 flex justify-between items-center"}>
-                                <div className={"flex items-center"}>
-                                    <span className={"font-iranSans text-sm text-carbon-main"}>مجموع</span>
-                                </div>
-                                <div className={"font-iranSans text-sm text-carbon-main flex items-center"}>
-                                    {toFarsiNumber(priceFormatting(totalPrice))}
-                                    <span className={"text-xs text-carbon-light mr-[4px]"}> تومان</span>
-                                </div>
-                            </div>
-                            <div className={clsx( !resInfo.tax && !resInfo.packagePrice ? "hidden" : "h-8 flex justify-between items-center")}>
-                                <div className={"flex items-center"}>
-                                    <span className={"text-carbon-light text-sm font-iranSans"}>{resInfo.tax > 0 ? "مالیات" : resInfo.packagePrice > 0 ? "هزینه بسته بندی" : ""}</span>
-                                </div>
-                                <div className={"font-iranSans text-sm text-carbon-main flex items-center"}>
-                                    {resInfo.tax > 0 ? toFarsiNumber(priceFormatting(resInfo.tax)) : resInfo.packagePrice > 0 ? toFarsiNumber(priceFormatting(resInfo.packagePrice)) : ""}
-                                    <span className={"text-xs text-carbon-light mr-[4px]"}> تومان</span>
-                                </div>
-                            </div>
-                            <div className={"h-8 flex justify-between items-center"}>
-                                <div className={"flex items-center"}>
-                                    <span className={"text-carbon-light text-sm font-iranSans"}>هزینه ارسال</span>
-                                </div>
-                                <div className={"font-iranSans text-sm text-carbon-main flex items-center"}>
-                                    {toFarsiNumber(priceFormatting(resInfo.delivery.price))}
-                                    <span className={"text-xs text-carbon-light mr-[4px]"}> تومان</span>
-                                </div>
-                            </div>
-                        </div>
-                        {
-                            isaActiveCoupon &&
-                            <div ref={activeCouponRef} className={"border-b border-b-carbon-alphaLight flex justify-between items-center"}>
-                                <div className={"my-4 flex-[1_0_auto] max-w-[calc(100%-2rem)]"}>
-                                    <div className={"mb-1 font-iRANSansBold text-xs text-accent2-main"}>
-                                        {isaActiveCoupon.split("/")[0]}
-                                    </div>
-                                    <div className={"font-iranSans text-xs text-carbon-main"}>
-                                        {isaActiveCoupon.split("/")[1]}
-                                    </div>
-                                </div>
-                                <button onClick={handleRemoveCoupon} className={"flex-[0_0_2rem] hover:bg-inactive-main py-[1px] px-[6px] min-w-8 bg-clip-padding rounded-full w-auto h-8 transition-socialFooter inline-flex items-center justify-center"}>
-                                    <Image src={"/icons/delete-gray.svg"} width={12} height={14} alt={"icon"}/>
-                                </button>
-                            </div>
-                        }
-                        <div className={"h-12 flex justify-between items-center"}>
-                            <p className={"font-iRANSansBold text-sm text-carbon-main"}>قابل پرداخت</p>
-                            <div className={"inline-flex items-center font-iRANSansBold text-sm text-carbon-main"}>
-                                {toFarsiNumber(priceFormatting(finalPrice))}
-                                <span className={"font-iranSans text-xs text-carbon-light mr-[4px]"}> تومان</span>
-                            </div>
-                        </div>
-                        <div className={"border-surface-dark border-[0.0625rem] mt-4 p-3 shadow-sp-small flex justify-between items-center rounded-[8px]"}>
-                            <div className={"flex items-baseline"}>
-                                <p className={"ml-1 font-iRANSansBold text-base text-carbon-main"}>{toFarsiNumber(randomClubPoint)}</p>
-                                <p className={"font-iranSans text-xs text-carbon-main"}>امتیاز از این سفارش دریافت خواهید کرد.</p>
-                            </div>
-                            <Image src={"/icons/club.svg"} width={17} height={21} alt={"icon"} className={"mr-1"}/>
-                        </div>
+                        <CartBill
+                            basket={basket}
+                            totalDiscount={totalDiscount}
+                            resInfo={resInfo}
+                            totalPrice={totalPrice}
+                        />
                     </Suspense>
                     <textarea placeholder={"توضیحات سفارش..."} className={"font-iranSans overflow-auto resize-y border-carbon-alphaMedium border-[0.09375rem] rounded-md text-carbon-main p-3 mb-[calc(1.75rem)] mt-4 w-full min-h-[calc(6rem)]"}/>
                     <div className={"bg-surface-light sticky bottom-0 w-full h-[7.5rem]"}>
