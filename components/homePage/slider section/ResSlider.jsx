0181d0dfffb6dc6a1d74a451516a6082292cc937
Mobina <mobina.ysf203@gmail.com>
2024-04-04 19:51:42 +0330

    fix (ResSlider): fixed buttons action and css

--- components/homePage/slider section/ResSlider.jsx
+++ components/homePage/slider section/ResSlider.jsx
@@ -25,13 +25,21 @@ export const ResSlider = ({children}) => {
     const swiperRef = useRef(null);
 
     const handleNext = () => {
-        swiperRef.current.swiper.slideNext();
-        setIsFirst(false);
+        if (children.length <= 5) {
+            swiperRef.current.swiper.slideNext();
+            if (swiperRef.current.swiper.isEnd) {
+                setIsFirst(false);
+            }
+        }
     }
 
     const handlePrev = () => {
-        swiperRef.current.swiper.slidePrev()
-        setIsFirst(true);
+        if (children.length <= 5) {
+            swiperRef.current.swiper.slidePrev();
+            if (swiperRef.current.swiper.isBeginning) {
+                setIsFirst(true);
+            }
+        }
 
     }
 
@@ -50,13 +58,13 @@ export const ResSlider = ({children}) => {
             {
                 children.length > 3 ?
                     <>
-                        <div className={clsx(isFirst && "opacity-100 scale-100","cursor-pointer opacity-0 scale-0 hidden sp-laptop:inline restaurantSections-button-transition h-8 z-10  bottom-0 top-[55%] absolute -left-6")}>
-                            <button disabled={children.length < 5} onClick={handleNext} className={"text-lg font-iRANSansBold shadow-sp-medium bg-clip-padding bg-surface-light text-accent-main inline-flex justify-center items-center transition-socialFooter box-border w-12 h-12 border-spBorder border-solid border-accent-alphaLight rounded-[50%]"}>
+                        <div className={clsx(isFirst && "opacity-100 scale-100","active:opacity-0 cursor-pointer opacity-0 scale-0 hidden sp-laptop:inline restaurantSections-button-transition h-8 z-10  bottom-0 top-[55%] absolute -left-6")}>
+                            <button onClick={handleNext} className={"text-lg font-iRANSansBold shadow-sp-medium bg-clip-padding bg-surface-light text-accent-main inline-flex justify-center items-center transition-socialFooter box-border w-12 h-12 border-spBorder border-solid border-accent-alphaLight rounded-[50%]"}>
                                 <Image src={"/icons/arrow-left-pink.svg"} width={9} height={16} alt={"icon"}/>
                             </button>
                         </div>
-                        <div className={clsx( !isFirst && "opacity-100 scale-100" ,"cursor-pointer hidden sp-laptop:inline restaurantSections-button-transition opacity-0 scale-0 h-8 z-10 top-[55%] bottom-0 absolute -right-6")}>
-                            <button disabled={children.length < 5} onClick={handlePrev} className={"inline-flex justify-center items-center transition-socialFooter box-border w-12 h-12 text-lg font-iRANSansBold shadow-sp-medium bg-clip-padding bg-surface-light text-accent-main border-spBorder border-solid border-accent-alphaLight rounded-[50%]"}>
+                        <div className={clsx( !isFirst && "opacity-100 scale-100" ,"active:opacity-0 cursor-pointer hidden sp-laptop:inline restaurantSections-button-transition opacity-0 scale-0 h-8 z-10 top-[55%] bottom-0 absolute -right-6")}>
+                            <button onClick={handlePrev} className={"inline-flex justify-center items-center transition-socialFooter box-border w-12 h-12 text-lg font-iRANSansBold shadow-sp-medium bg-clip-padding bg-surface-light text-accent-main border-spBorder border-solid border-accent-alphaLight rounded-[50%]"}>
                                 <Image src={"/icons/arrow-right-pink.svg"} width={9} height={16} alt={"icon"}/>
                             </button>
                         </div>
