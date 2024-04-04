8c56c0d8302a925806e1452afc1810d3c7cdbb69
Mobina <mobina.ysf203@gmail.com>
2024-04-04 04:59:44 +0330

    chore (): edit development texts

--- components/header/SearchBox.jsx
+++ components/header/SearchBox.jsx
@@ -2,9 +2,9 @@
 import Image from "next/image";
 import {useDispatch} from "react-redux";
 import {handleOpenModal} from "@/redux/features/headerSearchSlice";
-import {usePathname} from "next/navigation";
+import {useParams, usePathname} from "next/navigation";
 
-export const SearchBox = () => {
+export const SearchBox = ({restaurants}) => {
     const pathName = usePathname();
     const isHomePage = pathName === "/";
     const isResCategory = pathName === "/restaurant";
@@ -12,11 +12,17 @@ export const SearchBox = () => {
     const dispatch = useDispatch();
     const handleOpenSearch = () => {dispatch(handleOpenModal())}
 
+    let whichResPage;
+    const params = useParams()
+    if (params.restaurantId) {
+        whichResPage = restaurants.find((res) => res.id === Number(params.restaurantId[0])).name;
+    }
+
     return (
         <div onClick={handleOpenSearch} className={"header-searchBox w-[10.5625rem] items-center flex absolute left-0 box-border h-12 p-4 rounded-[0.625rem]"}>
             <Image src={"/icons/search.svg"} width={17} height={17} alt={"icon"} className={"opacity-[0.45] ml-2"}/>
             <p className={"text-inactive-dark text-base font-iranSans ml-2 max-[959px]:hidden"}>
-                {isHomePage ? "جست‌وجو در اسنپ‌فود" : isResCategory || isPartyPage ? "جست‌وجو در رستوران" : "جست‌وجو در "}
+                {isHomePage ? "جست‌وجو در اسنپ‌فود" : isResCategory || isPartyPage ? "جست‌وجو در رستوران" : `جست‌وجو در ${whichResPage}`}
             </p>
         </div>
     )
